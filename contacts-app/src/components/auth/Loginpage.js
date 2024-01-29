b51d15b831134319f29cac5d7b112b716e1026d4
IrtezaAli12 <irtezahasnain@gmail.com>
2024-01-29 20:26:50 +0500

    Added common components, Auth and Routing

---
+++ contacts-app/src/components/auth/Loginpage.js
@@ -0,0 +1,34 @@
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/esm/Button";
+
+function LoginPage() {
+    return (
+        <Container>
+            <Row className="px-4 my-5">
+                <Col><h1>Login</h1></Col>
+            </Row>
+            <Row className="px-4 my-5">
+                <Col sm={6}>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">Login &gt;&gt;</Button>&nbsp;
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default SiteFooter;
